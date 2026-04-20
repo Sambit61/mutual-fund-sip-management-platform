392adfbe93a40dc43335ddad923d427211f0569d
Sambit61 <sambitbarik05@gmail.com>
2026-04-20 14:59:14 +0530

    fixed deployment

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ app.use((req, res, next) => {
   console.log("INCOMING REQUEST:", req.method, req.url);
   next();
 });
-app.use(cors());
+app.use(cors({
+  origin: "https://mfsip.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 runSIPScheduler();
