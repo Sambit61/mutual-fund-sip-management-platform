6a9b558f7cf7fd7b3a8bbb86e72a931f55c20fcd
Sambit61 <sambitbarik05@gmail.com>
2026-06-19 08:07:16 +0530

    fixed gmail issue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,9 +6,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // Force IPv4
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
